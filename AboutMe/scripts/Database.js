2e9793aa14136f9be07c5f62644ce9bdf9ded2a3
MadiRosewood <Madison.Rosewood.anderson@gmail.com>
2024-10-30 03:00:32 -0400

    Add files via upload

---
+++ AboutMe/scripts/Database.js
@@ -0,0 +1,13 @@
+const data = {
+    "users": {
+        "Madi": {
+            "name": "Madi",
+            "quote": "Live and let live.",
+            "prn": "She/Her",
+            "age": 22,
+            "ht": "5'0\"",
+            "bio": "A short bio about Madi.",
+            "image": "https://tr.rbxcdn.com/30DAY-AvatarHeadshot-5AA80D0F091CF9C283BF0F516A2562FE-Png/150/150/AvatarHeadshot/Webp/noFilter" // Add an image URL here
+        }
+    }
+};
