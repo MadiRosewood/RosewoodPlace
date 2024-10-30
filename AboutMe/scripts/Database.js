80545f14f989fc2992e5b44a1665b5c5935c10eb
MadiRosewood <Madison.Rosewood.anderson@gmail.com>
2024-10-30 03:26:51 -0400

    Update Database.js

--- AboutMe/scripts/Database.js
+++ AboutMe/scripts/Database.js
@@ -2,12 +2,21 @@ const data = {
     "users": {
         "Madi": {
             "name": "Madi",
-            "quote": "Live and let live.",
+            "quote": "Meow!",
             "prn": "She/Her",
             "age": 22,
             "ht": "5'0\"",
-            "bio": "A short bio about Madi.",
+            "bio": "A short bio about Madi, shorter than madi.",
             "image": "https://tr.rbxcdn.com/30DAY-AvatarHeadshot-5AA80D0F091CF9C283BF0F516A2562FE-Png/150/150/AvatarHeadshot/Webp/noFilter" // Add an image URL here
+        },
+        "Moon": {
+            "name": "Moon",
+            "quote": "Expect the unexpected!",
+            "prn": "She/Her",
+            "age": 20,
+            "ht": "N/A",
+            "bio": "A short bio about Moon.",
+            "image": "https://placehold.co/128x128" // Add an image URL here
         }
     }
 };
